33ad36548ea7e8e9d8636bfbf50b276e2106d24a
Adeniyi <teebarg01@gmail.com>
2024-11-02 22:07:38 +0100

    Use supabase dc

--- backend/models/category.py
+++ backend/models/category.py
@@ -1,5 +1,6 @@
 from sqlmodel import Field
 
+# from pydantic import BaseModel as PDBModel, computed_field
 from models.base import BaseModel
 
 
@@ -16,3 +17,27 @@ class CategoryCreate(CategoryBase):
 # Properties to receive via API on update, all are optional
 class CategoryUpdate(CategoryBase):
     pass
+
+
+# class CategoryPublic(CategoryBase):
+#     id: int
+#     slug: str
+#     parent_id: Optional[int]
+
+#     @computed_field
+#     @property
+#     def parent(self) -> Optional["Category"]:
+#         """
+#         Computed property to get the parent category.
+#         """
+#         if not self.parent_id:
+#             return None
+
+#         # Fetch parent category from Supabase
+#         response = db.table("categories")
+#                   .select("*")
+#                   .eq("id", self.parent_id).single().execute()
+#         if not response.data:
+#             return None
+
+#         return Category(**response.data)
