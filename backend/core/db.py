1d5f07a4a0c470c3595adc81961b7d1d0a8a9e7f
Adeniyi <teebarg01@gmail.com>
2024-11-02 22:20:12 +0100

    Upadte

--- backend/core/db.py
+++ backend/core/db.py
@@ -1,17 +1,17 @@
-from functools import lru_cache
-from typing import Annotated
+# from functools import lru_cache
+# from typing import Annotated
 
-from fastapi import Depends
-from supabase import Client, create_client
+# from fastapi import Depends
+# from supabase import Client, create_client
 
-from core.config import settings
-
-
-@lru_cache()
-def get_supabase_client() -> Client:
-    return create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
+# from core.config import settings
 
 
-Supabase = Annotated[Client, Depends(get_supabase_client)]
+# @lru_cache()
+# def get_supabase_client() -> Client:
+#     return create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
 
-db = get_supabase_client()
+
+# Supabase = Annotated[Client, Depends(get_supabase_client)]
+
+# db = get_supabase_client()
