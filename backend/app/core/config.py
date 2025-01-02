aeb233ed89ff955aabb2d0b685fbde31ec7e85e6
Adeniyi <teebarg01@gmail.com>
2025-01-02 19:56:18 +0100

    Update config

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -114,7 +114,7 @@ class Settings(BaseSettings):
 
     model_config = SettingsConfigDict(
         # Use top level .env file (one level above ./backend/)
-        env_file="../.env",
+        env_file=".env",
         env_ignore_empty=True,
         extra="ignore",
     )
