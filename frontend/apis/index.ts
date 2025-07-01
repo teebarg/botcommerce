333d280fb319cbeba3fbfc89e616a96aee88bb70
Adeniyi <teebarg01@gmail.com>
2025-07-01 10:26:40 +0100

    update

--- frontend/apis/index.ts
+++ frontend/apis/index.ts
@@ -2,12 +2,10 @@ import { authApi } from "./auth";
 import { productApi } from "./product";
 import { userApi } from "./user";
 import { reviewsApi } from "./review";
-import { paymentApi } from "./payment";
 import { adminApi } from "./admin";
 
 export const api = {
     auth: authApi,
-    payment: paymentApi,
     product: productApi,
     review: reviewsApi,
     user: userApi,
