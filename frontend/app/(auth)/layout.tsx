a87cc13127eaee857d1c0875a731637eebfeae6a
Adeniyi <teebarg01@gmail.com>
2025-08-31 20:41:23 +0100

    update

--- frontend/app/(auth)/layout.tsx
+++ frontend/app/(auth)/layout.tsx
@@ -8,7 +8,7 @@ export default async function AuthLayout({ children }: { children: React.ReactNo
 
     return (
         <div className="h-screen flex flex-col bg-content2">
-            <nav className="w-full max-w-md px-6 py-4 fixed top-0">
+            <nav className="w-full max-w-md p-6 fixed top-0">
                 <div className="flex items-center justify-center">
                     <div className="flex items-center space-x-2">
                         <div className="w-8 h-8 bg-linear-to-br from-primary to-secondary rounded-lg flex items-center justify-center">
