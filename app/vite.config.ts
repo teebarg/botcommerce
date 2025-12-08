129bbe2f23b96466dbad8f6986fc8a6f1d29e753
Adeniyi <teebarg01@gmail.com>
2025-12-08 16:10:41 +0100

    Add nitro for deployment

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,6 +3,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
     server: {
@@ -18,6 +19,7 @@ export default defineConfig({
             projects: ["./tsconfig.json"],
         }),
         tanstackStart(),
+        nitro(),
         viteReact(),
     ],
 });
