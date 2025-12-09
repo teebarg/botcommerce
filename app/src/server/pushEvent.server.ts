0239122078bdd5d997efd8726f41b27ea5a03e15
Adeniyi <teebarg01@gmail.com>
2025-12-09 15:13:37 +0100

    fix push event endpoint

--- app/src/server/pushEvent.server.ts
+++ app/src/server/pushEvent.server.ts
@@ -1,11 +1,17 @@
+import { Message } from "@/schemas";
+import { api } from "@/utils/fetch-api";
 import { createServerFn } from "@tanstack/react-start";
+import { z } from "zod";
 
-export const pushEventFn = createServerFn({ method: "POST" }).handler(async ({ data }) => {
-    const backendRes = await fetch(`${process.env.API_URL}/api/notification/push-event`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data),
+export const pushEventFn = createServerFn({ method: "POST" })
+    .inputValidator(
+        z.object({
+            subscriberId: z.string(),
+            notificationId: z.string(),
+            eventType: z.enum(["DELIVERED", "OPENED", "CLICKED", "DISMISSED"]),
+            timestamp: z.string(),
+        })
+    )
+    .handler(async ({ data }) => {
+        return await api.post<Message>("/notification/push-event", data);
     });
-
-    return backendRes.json();
-});
