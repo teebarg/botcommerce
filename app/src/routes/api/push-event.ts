7b2ebea9f33e02e44ca8b883612d24875fa0a343
Adeniyi <teebarg01@gmail.com>
2025-12-10 11:54:46 +0100

    fix push notifications tracker

--- app/src/routes/api/push-event.ts
+++ app/src/routes/api/push-event.ts
@@ -1,12 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { pushEventFn } from "@/server/pushEvent.server";
+import { Message } from "@/schemas";
+import { api } from "@/utils/fetch-api";
 
 export const Route = createFileRoute("/api/push-event")({
     server: {
         handlers: {
             POST: async ({ request }) => {
                 const data = await request.json();
-                const result = await pushEventFn({ data });
+                const result = await api.post<Message>("/notification/push-event", data);
                 return Response.json(result);
             },
         },
