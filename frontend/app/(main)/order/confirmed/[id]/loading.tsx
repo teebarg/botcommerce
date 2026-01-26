cc0173aeb5f0cc1e17b66a06f6ef3215852daed0
Adeniyi <teebarg01@gmail.com>
2026-01-26 09:30:56 +0100

    Remove frontend dir

--- frontend/app/(main)/order/confirmed/[id]/loading.tsx
+++
@@ -1,5 +0,0 @@
-import ComponentLoader from "@/components/component-loader";
-
-export default function Loading() {
-    return <ComponentLoader className="min-h-screen" />;
-}
--- frontend/components/LazyClient.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-export const Featured = dynamic(() => import("@/components/store/home/featured"), { ssr: false });
-export const Trending = dynamic(() => import("@/components/store/home/trending"), { ssr: false });
-export const RecentlyViewedSection = dynamic(() => import("@/components/store/home/recently-viewed"), { ssr: false });
-export const NewArrivals = dynamic(() => import("@/components/store/home/arrival"), { ssr: false });
-export const NewsletterSection = dynamic(() => import("@/components/store/landing/newsletter-section"), { ssr: false });
