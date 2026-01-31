76872f73a3129add6242af964bead9ee84753ad7
Adeniyi <teebarg01@gmail.com>
2026-01-31 07:56:56 +0100

    update

--- app/src/components/store/landing/category-section.tsx
+++ app/src/components/store/landing/category-section.tsx
@@ -10,7 +10,7 @@ const CategoriesSection: React.FC = () => {
     return (
         <div className="bg-linear-to-b from-background to-card/30">
             <div className="max-w-8xl mx-auto px-4 py-8 w-full text-left md:text-center">
-                <h2 className="text-lg md:text-3xl font-bold mb-2 mt-6">Shop by category</h2>
+                <h2 className="text-lg md:text-3xl font-bold mb-4 mt-6">Shop by category</h2>
                 {isLoading ? (
                     <ComponentLoader className="h-48" />
                 ) : (
