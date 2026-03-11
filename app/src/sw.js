ff664b80703e33255a5947969f1e88ed80a1fd87
Adeniyi <teebarg01@gmail.com>
2026-03-11 18:23:08 +0100

    update

--- app/src/sw.js
+++ app/src/sw.js
@@ -79,7 +79,7 @@ registerRoute(
 registerRoute(
     ({ request, url }) => {
         const isNavigation = request.mode === "navigate";
-        const isApiRequest = url.pathname.startsWith("/api/") || url.hostname === "api.revoque.com.ng" || url.hostname === "api-shop.localhost";
+        const isApiRequest = url.pathname.startsWith("/api/") || url.hostname === "api.revoque.com.ng" || url.hostname === "api.shop.lvh.me";
 
         return isNavigation || isApiRequest;
     },
