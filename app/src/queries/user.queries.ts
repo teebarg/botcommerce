09468943749f09a82dd18b07e7fcb1557bd72584
Adeniyi <teebarg01@gmail.com>
2026-06-16 23:33:05 +0100

    reduce feed seed

--- app/src/queries/user.queries.ts
+++ app/src/queries/user.queries.ts
@@ -41,7 +41,7 @@ export const meTxnsQuery = () =>
 export const productFeedQuery = (params: FeedParams) =>
     queryOptions({
         queryKey: ["products", "list", params],
-        queryFn: () => getProductsFeedFn({ data: { ...params, feed_seed: Number((Math.random() * 0.5).toFixed(1)) } }),
+        queryFn: () => getProductsFeedFn({ data: { ...params, feed_seed: Number((Math.random() * 0.3).toFixed(2)) } }),
     });
 
 export const catalogFeedQuery = (params: CatalogFeedParams) =>
