62de74a28f1b73fc1ba17d88429ba53f32f07a62
Adeniyi <teebarg01@gmail.com>
2026-07-03 05:31:48 +0100

    fix desktop fixed header padding

--- app/src/routes/_mainLayout.tsx
+++ app/src/routes/_mainLayout.tsx
@@ -16,7 +16,7 @@ function MainLayoutComponent() {
     return (
         <div className="flex flex-col min-h-screen">
             <StoreNavbar />
-            <main className="flex-1 flex flex-col pt-16 md:pt-0 pb-8">
+            <main className="flex-1 flex flex-col pt-[var(--nav-height)] pb-8">
                 <Outlet />
             </main>
             <Footer />
